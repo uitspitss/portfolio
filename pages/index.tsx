8ea5c4dab6670b345ac7c63ff4f92ac970c7f789
uitspitss <uitspitss@gmail.com>
2019-03-19 04:05:00 +0900

    Add files for SSR of material-ui

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,13 +1,8 @@
 import * as React from 'react';
 import Link from 'next/link';
-import Head from '../components/head';
-import Nav from '../components/nav';
 
 const Home = () => (
   <div>
-    <Head title="Home" />
-    <Nav />
-
     <div className="hero">
       <h1 className="title">Welcome to Next!</h1>
       <p className="description">
