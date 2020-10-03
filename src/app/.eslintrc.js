1ffe3683143829d6e3a99b2d674bd54e2974e73c
uitspitss <3781626+uitspitss@users.noreply.github.com>
2020-10-03 15:19:32 +0900

    Update dev config files

--- src/app/.eslintrc.js
+++ src/app/.eslintrc.js
@@ -1,19 +1,28 @@
 module.exports = {
-  root: true,
-  env: {
-    browser: true,
-    es6: true,
-    node: true,
+  "root": true,
+  "plugins": ["jest", "react"],
+  "env": {
+    "es2020": true,
+    "jest/globals": true
   },
-  extends: [
-    'eslint:recommended',
-    'plugin:react/recommended',
-    'plugin:@typescript-eslint/recommended',
-    'plugin:prettier/recommended',
-    'prettier/@typescript-eslint',
-    'prettier/react',
+  "extends": [
+    "eslint:recommended",
+    "plugin:prettier/recommended",
+    "plugin:@typescript-eslint/eslint-recommended",
+    "plugin:@typescript-eslint/recommended",
+    "plugin:react-hooks/recommended"
   ],
-  parser: '@typescript-eslint/parser',
-  plugins: ['react', '@typescript-eslint'],
-  rules: {},
-};
+  "rules": {
+    "no-mixed-operators": "error",
+    "no-console": "off",
+    "no-undef": "off",
+    "react/jsx-uses-vars": 1,
+    "@typescript-eslint/explicit-function-return-type": "off",
+    "@typescript-eslint/explicit-module-boundary-types": "off",
+    "@typescript-eslint/no-non-null-assertion": "off"
+  },
+  "parserOptions": {
+    "project": "./tsconfig.json"
+  },
+  "parser": "@typescript-eslint/parser"
+}
